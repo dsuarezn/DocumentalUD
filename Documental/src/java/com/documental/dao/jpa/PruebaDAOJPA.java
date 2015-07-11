3058e8cffa42eb4d827564d31b722952cf0c59f2
dsuarezn <d.alexander.suarez@gmail.com>
2015-07-11 04:38:25 -0500

    Merge origin/branch_trabajo into branch_trabajo
    Conflicts:
    	Documental/nbproject/project.properties
    	Documental/src/java/com/documental/dao/DAOFactory.java
    	Documental/src/java/com/documental/dao/jpa/DAOJPAFactory.java
    	Documental/web/WEB-INF/faces-config.xml

---
+++ Documental/src/java/com/documental/dao/jpa/PruebaDAOJPA.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.documental.dao.jpa;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class PruebaDAOJPA {
+    
+}
